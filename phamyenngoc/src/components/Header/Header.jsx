f66100848d8c291e86269f9918276ededf21cdd5
Thien Quang <toiquang24@gmail.com>
2024-04-13 09:18:03 +0700

    Update header

--- phamyenngoc/src/components/Header/Header.jsx
+++ phamyenngoc/src/components/Header/Header.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { Dropdown, Navbar } from 'flowbite-react';
 import avatar from '../../assets/image/avatar/avatar.png'
-import { BsCart4 } from "react-icons/bs";
 import { BsInstagram } from "react-icons/bs";
 import { AiOutlineFacebook } from "react-icons/ai";
 import { BsTwitter } from "react-icons/bs";
@@ -10,8 +9,6 @@ import { AiOutlineYoutube } from "react-icons/ai";
 import { CiLocationOn } from "react-icons/ci";
 import { AiOutlineMail } from "react-icons/ai";
 function Header() {
-  const navigate = useNavigate();
-  const [hasUser, setHasUser] = useState(true);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [isVisible, setIsVisible] = useState(true);
 
@@ -34,17 +31,10 @@ function Header() {
     };
   }, [isVisible, prevScrollPos]);
 
-  const handleLogOut = () => {
-    //setAuth({});
-    localStorage.removeItem('auth');
-    setHasUser(false);
-    navigate('/');
-  };
   return (
     <div
       className={`sticky mb-8 max-w-[1200px] top-0 left-0 right-0 w-full z-50  mx-auto transition-transform transform duration-300 ${isVisible ? 'translate-y-0' : '-translate-y-full'
-        }`}
-    >
+        }`} >
       <div className="w-full flex justify-between p-2 bg-white text-black bg-opacity-50">
         <div className="flex justify-center items-center font-sans text-lg">
           <ul className="flex justify-center items-center">
@@ -64,8 +54,6 @@ function Header() {
             </li>
           </ul>
         </div>
-
-
         <div className="text-black flex justify-center items-center font-sans text-lg">
           <ul className="flex justify-center items-center cursor-pointer gap-2">
             <li className="flex justify-center items-center mr-1 ">
@@ -91,108 +79,57 @@ function Header() {
           </ul>
         </div>
       </div>
-      <div className="w-full ">
-        <div className="bg-white rounded-lg flex items-center justify-center">
-          <div className="w-full border-slate-300 border-2 rounded-lg">
-            <Navbar className="w-full py-2 z-50 shadow-sm bg-opacity-10 rounded-lg">
-              <Navbar.Brand>
-                <div className="avatar">
-                  <div className="w-14 rounded-full">
-                    <img src={require(`./web_logo.png`)} alt="" />
-                  </div>
-                </div>
-                <span style={{ fontFamily: '"inter", sans-serif', fontWeight: 'bold', fontSize: '25px' }} className="self-center whitespace-nowrap text-xl font-semibold text-black">
-                  BURGER N' BEER
-                </span>
-              </Navbar.Brand>
-
-              <Navbar.Collapse className="text-black">
-                <div style={{ fontFamily: '"inter", sans-serif', fontSize: '20px', marginRight: '30px' }} className="text-black hover:text-red-500">
-                  <Link to="/home">Home</Link>
-                </div>
-
-                <div style={{ fontFamily: '"inter", sans-serif', fontSize: '20px', marginRight: '30px' }} className="text-black hover:text-red-500">
-                  <Link to="/combo">Shop</Link>
-                </div>
-
-
-                <div style={{ fontFamily: '"inter", sans-serif', fontSize: '20px', marginRight: '30px' }} className="text-black hover:text-red-500">
-                  <Link to="/https://www.siliconii.com/">Location</Link>
-                </div>
-                <div style={{ fontFamily: '"inter", sans-serif', fontSize: '20px' }} className="text-black hover:text-red-500">
-                  <Link to="/contact">Contact Us</Link>
-                </div>
-              </Navbar.Collapse>
-              {hasUser ? (
-                <div className="flex md:order-2 text-black">
-                  <div className="flex items-center justify-center">
-                    <Link to="/cart">
-                      <div className="w-10 h-10 rounded-full border border-black flex items-center justify-center mr-2 hover:bg-btnprimary hover:text-[#B4E9D6] ">
-                        <BsCart4 />
-                      </div>
-                    </Link>
-                  </div>
-                  <Dropdown
-                    arrowIcon={true}
-                    inline
-                    label={
-                      <div className="avatar">
-                        <div className="w-10 rounded-full">
-                          <img src={avatar} alt="" />
-                        </div>
-                      </div>
-                    }
-                  >
-                    <Dropdown.Header>
-                      <span className="flex items-center justify-center text-xl m-2 ">
-                        Pham Thi Yen Ngoc
-                      </span>
-                      <span className="block truncate text-sm font-medium">
-                        phamyenngoc.02@gmail.com
-                      </span>
-                    </Dropdown.Header>
-                    {/* {auth.role === "admin" && ( */}
-                    <Link to="/admin">
-                      <Dropdown.Item>DASHBOARD</Dropdown.Item>
-                    </Link>
-                    {/* )} */}
-
-                    <Link to="/user">
-                      <Dropdown.Item>PROFILE</Dropdown.Item>
-                    </Link>
-                    <Dropdown.Divider />
-                    <Link to="/home" onClick={handleLogOut}>
-                      <Dropdown.Item>LOGOUT</Dropdown.Item>
-                    </Link>
-                    { }
-                  </Dropdown>
-                </div>
-              ) : (
-                <Navbar.Collapse>
-                  <div className="flex justify-center items-center">
-                    <ul className="flex justify-center items-center ">
-                      <li>
-                        <Link to="/login">
-                          <button className="btn btn-outline btn-success">
-                            LOGIN
-                          </button>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/register">
-                          <button className="btn btn-info ml-4">
-                            REGISTER
-                          </button>
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                </Navbar.Collapse>
-              )}
-            </Navbar>
+      <Navbar fluid rounded className='border rounded-lg'>
+        <Navbar.Brand>
+          <div className="avatar">
+            <div className="w-16 rounded-full">
+              <img src={require(`./web_logo.png`)} alt="" />
+            </div>
           </div>
+          <span className="self-center whitespace-nowrap font-bold text-black text-xl">
+            BURGER N' BEER
+          </span>
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <div className="avatar">
+                <div className="w-14 rounded-full">
+                  <img src={avatar} alt="" />
+                </div>
+              </div>
+            }
+          >
+            <Dropdown.Header>
+              <span className="flex items-center justify-center text-xl m-2 ">
+                Pham Thi Yen Ngoc
+              </span>
+              <span className="block truncate text-sm font-medium">
+                phamyenngoc.02@gmail.com
+              </span>
+            </Dropdown.Header>
+            <Link to="/admin">
+              <Dropdown.Item>DASHBOARD</Dropdown.Item>
+            </Link>
+            <Link to="/user">
+              <Dropdown.Item>PROFILE</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+          <Navbar.Toggle />
         </div>
-      </div>
+        <Navbar.Collapse>
+          <Navbar.Link href="/">
+            HOME
+          </Navbar.Link>
+          <Navbar.Link href="/combo">SHOP</Navbar.Link>
+          <Navbar.Link href="/https://www.siliconii.com/">LOCATION</Navbar.Link>
+          <Navbar.Link href="/contact">CONTACT US</Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
--- phamyenngoc/tailwind.config.js
+++ phamyenngoc/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{html,js,jsx}", flowbite.content(),],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), flowbite.plugin(),],
 }
